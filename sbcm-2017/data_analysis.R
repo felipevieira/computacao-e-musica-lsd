ec59f785736dae24c38a623b4937254d5ae59240
Felipe Vieira <felipe29vieira@gmail.com>
2017-05-29 17:28:05 -0300

    Add aotm dataset

--- sbcm-2017/data_analysis.R
+++ sbcm-2017/data_analysis.R
@@ -8,8 +8,11 @@ library(gmodels)
 dados_vagalume = read_csv("Datasets/vagalume_playlists.csv", col_names=TRUE)
 dados_8tracks = read_csv("Datasets/8tracks_playlists.csv", col_names=TRUE)
 dados_playlists_net = read_csv("Datasets/playlists_net_playlists.csv", col_names=TRUE)
+dados_aotm = read_csv("Datasets/aotm_playlists.csv", col_names=TRUE)
+
 
 # Merging data
-full_data = rbind(dados_vagalume, dados_8tracks, dados_playlists_net)
+full_data = rbind(dados_vagalume, dados_8tracks, dados_playlists_net, dados_aotm)
 
 # Our analysis starts here :)
+length(unique(full_data$playlist_id))
