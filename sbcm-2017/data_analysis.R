5d167b088dbb6e61d120d4ecf0911cf49ad19a91
Felipe Vieira <felipe29vieira@gmail.com>
2017-05-29 17:10:21 -0300

    Add R analysis script

---
+++ sbcm-2017/data_analysis.R
@@ -0,0 +1,15 @@
+library(dplyr, warn.conflicts = F)
+library(readr)
+library(ggplot2)
+theme_set(theme_bw())
+library(gmodels)
+
+# Reading data from all sources
+dados_vagalume = read_csv("Datasets/vagalume_playlists.csv", col_names=TRUE)
+dados_8tracks = read_csv("Datasets/8tracks_playlists.csv", col_names=TRUE)
+dados_playlists_net = read_csv("Datasets/playlists_net_playlists.csv", col_names=TRUE)
+
+# Merging data
+full_data = rbind(dados_vagalume, dados_8tracks, dados_playlists_net)
+
+# Our analysis starts here :)
