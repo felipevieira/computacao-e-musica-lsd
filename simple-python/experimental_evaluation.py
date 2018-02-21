45fc08f7d1d0e848da97fd1f3dd3f7fc5a851e4c
Felipe Vieira <felipe29vieira@gmail.com>
2018-02-21 16:39:52 -0300

    Parametrizing experiment script

--- simple-python/experimental_evaluation.py
+++ simple-python/experimental_evaluation.py
@@ -6,47 +6,14 @@ import sys
 import os
 import ntpath
 import pickle
+import argparse
 
 from concurrent.futures import ThreadPoolExecutor
 
-<<<<<<< HEAD
-TRAINING_FILE_PATH = '/home/felipev/workspace/computacao-e-musica-lsd/simple-python/YTCdataset/listtrain'
-TESTING_FILE_PATH = '/home/felipev/workspace/computacao-e-musica-lsd/simple-python/YTCdataset/listtest'
 
-DATASET_HOME = '/home/felipev/workspace/computacao-e-musica-lsd/simple-python/YTCdataset'
-=======
-TRAINING_FILE_PATH = '/home/felipe/Desktop/YTCdataset/listtrain'
-TESTING_FILE_PATH = '/home/felipe/Desktop/YTCdataset/listtest'
-
-DATASET_HOME = '/home/felipe/Desktop/YTCdataset'
->>>>>>> 3a5e5a11fff90cf75416b4a1aa032075110f899f
-
-
-def write_chroma_series_to_file(file_path, chroma_series):
-    if not os.path.exists("%s/### Experiments ###/Chromas for Training Entries/%s" % (DATASET_HOME, file_path.split("/")[0])):
-        os.makedirs("%s/### Experiments ###/Chromas for Training Entries/%s" %
-                    (DATASET_HOME, file_path.split("/")[0]))
-    with open("%s/### Experiments ###/Chromas for Training Entries/%s.chroma" % (DATASET_HOME, file_path.strip()), "w+") as file:
-        pickle.dump(chroma_series, file)
-
-
-def load_chroma_series_from_file(label_file):
-    with open(file_path, "rb") as file:
-        return pickle.load(file)
-
-
-<<<<<<< HEAD
-LABEL_FILE = 'YTCdataset/listfiles'
-CHROMAS_FILE = 'YTCdataset/YTC.deepChroma.smoothed.all'
-=======
-LABEL_FILE = '/home/felipe/Desktop/ytc/listfiles'
-CHROMAS_FILE = '/home/felipe/Desktop/cens_aggregated=2fs-allsongs.txt'
->>>>>>> 3a5e5a11fff90cf75416b4a1aa032075110f899f
-
-
-def get_chroma_from_file(label):
-    label_file = open(LABEL_FILE, 'r')
-    chromas_file = open(CHROMAS_FILE, 'r')
+def get_chroma_from_file(dataset_path, chromas_file, label):
+    label_file = open(os.path.join(dataset_path, 'listfiles'), 'r')
+    chromas_file = open(os.path.join(dataset_path, chromas_file), 'r')
 
     LABELS = label_file.read().splitlines()
     CHROMAS = chromas_file.read().splitlines()
@@ -63,38 +30,39 @@ def get_chroma_from_file(label):
     return chroma_representation
 
 
-def load_time_series_for_train_set():
-    training_file = open(TRAINING_FILE_PATH, 'r')
+def load_time_series_for_train_set(dataset_path, chromas_file):
+    training_file = open(os.path.join(dataset_path, 'listtrain'), 'r')
     training_time_series = {}
 
     for entry in training_file.readlines():
 
         training_entry = entry.strip()
 
-        training_time_series[training_entry] = get_chroma_from_file(
-            training_entry)
+        training_time_series[training_entry] = get_chroma_from_file(dataset_path, chromas_file,
+                                                                    training_entry)
 
     training_file.close()
     return training_time_series
 
 
-def get_similarity_ranking_for_testing_entry(training_time_series, testing_entry):
+def get_similarity_ranking_for_testing_entry(dataset_path, chromas_file, training_time_series, testing_entry):
     print "Building ranking for: %s" % testing_entry
     similarity_ranking = {}
 
-    testing_time_series = get_chroma_from_file(testing_entry)
+    testing_time_series = get_chroma_from_file(
+        dataset_path, chromas_file, testing_entry)
 
     for training_entry in training_time_series.keys():
         similarity_ranking[training_entry] = SiMPle.similarity_by_simple(
             testing_time_series, training_time_series[training_entry])
 
-    write_ranking_to_file(testing_entry, sorted(
+    write_ranking_to_file(dataset_path, testing_entry, sorted(
         similarity_ranking.iteritems(), key=lambda (k, v): (v, k)))
 
 
-def write_ranking_to_file(testing_entry, dict_ranking):
-    ranking_file = open("%s/_RANKINGS_/%s" %
-                        (DATASET_HOME, testing_entry.replace("/", "|")), "w+")
+def write_ranking_to_file(dataset_path, testing_entry, dict_ranking):
+    ranking_file = open(os.path.join(
+        dataset_path, "_RANKINGS_", testing_entry.replace("/", "|")), "w+")
 
     for ranking_entry in dict_ranking:
         ranking_file.write("%s %.3f\n" % (ranking_entry[0], ranking_entry[1]))
@@ -142,39 +110,38 @@ def get_average_precision(ranking_file):
     return sum(average_precision_series) / 2
 
 
-def main_experiment(testing_file):
-    training_time_series = load_time_series_for_train_set()
+def main_experiment(dataset_path, chromas_file):
+    training_time_series = load_time_series_for_train_set(
+        dataset_path, chromas_file)
 
-    testing_file = open(testing_file, 'r')
+    testing_file = open(os.path.join(dataset_path, 'listtest'), 'r')
 
     entry_count = 0
 
-    # TODO: Parallelize ranking tasks
     with ThreadPoolExecutor(max_workers=5) as e:
         for entry in testing_file.readlines():
             testing_entry = entry.strip()
             entry_count += 1
 
-            e.submit(get_similarity_ranking_for_testing_entry,
+            e.submit(get_similarity_ranking_for_testing_entry, dataset_path, chromas_file,
                      training_time_series, testing_entry)
-    # wait for termination
 
 
-def main_metrics(ranking_folder):
+def main_metrics(dataset_path):
     f = []
     first_identifieds = []
     precisions_at_10 = []
     average_precisions = []
-    for (dirpath, dirnames, filenames) in os.walk(ranking_folder):
+    for (dirpath, dirnames, filenames) in os.walk(os.path.join(dataset_path, "_RANKINGS_")):
         f.extend(filenames)
 
     for file in f:
         first_identifieds.append(get_rank_first_identified(
-            "%s/%s" % (ranking_folder, file)))
+            os.path.join(dataset_path, "_RANKINGS_", file)))
         precisions_at_10.append(get_precision_at_10(
-            "%s/%s" % (ranking_folder, file)))
+            os.path.join(dataset_path, "_RANKINGS_", file)))
         average_precisions.append(get_average_precision(
-            "%s/%s" % (ranking_folder, file)))
+            os.path.join(dataset_path, "_RANKINGS_", file)))
 
     print "%s: %.3f" % ("MR1", sum(first_identifieds) / float(len(first_identifieds)))
     print "%s: %.3f" % ("P@10", sum(precisions_at_10) / float(len(precisions_at_10)))
@@ -182,7 +149,20 @@ def main_metrics(ranking_folder):
 
 
 if __name__ == '__main__':
-    if (sys.argv[1] == "run_experiment"):
-        main_experiment(sys.argv[2])
-    elif (sys.argv[1] == "run_evaluation"):
-        main_metrics(sys.argv[2])
+    parser = argparse.ArgumentParser(
+        description='Foo')
+    parser.add_argument('dataset_path', metavar='dataset_path',
+                        help='The path of the dataset containing all songs and summary files (listfiles, listtrain, listtest)')
+    parser.add_argument('phase', metavar='phase', choices=['experiment', 'evaluation'],
+                        help='Wether experimenting or evaluating experiment results')
+
+    parser.add_argument('--chromas_file', metavar='chromas_file',
+                        help='The path of the file containing the chromas whose subsequences will be compared (inside dataset path)')
+
+    parsed_args = parser.parse_args()
+
+    if (parsed_args.phase == "experiment"):
+        main_experiment(parsed_args.dataset_path,
+                        parsed_args.chromas_file if parsed_args.chromas_file else "YTC.deepChroma.smoothed.all")
+    elif (parsed_args.phase == "evaluation"):
+        main_metrics(parsed_args.dataset_path)
