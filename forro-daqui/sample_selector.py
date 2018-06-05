a2799d4650f027a2d4ce2b829a6eb34db9b064ea
Felipe Vieira <felipe20vieira@gmail.com>
2018-06-05 19:14:22 -0300

    New task html

--- forro-daqui/sample_selector.py
+++ forro-daqui/sample_selector.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 import re
 import random
 
+from pyAudioAnalysis import audioAnalysis
+from pydub import AudioSegment
 
-def split_albums_by_decade(path):
-    DECADES = [(1950, 1959), (1960, 1969), (1970, 1979),
+DECADES = [(1950, 1959), (1960, 1969), (1970, 1979),
                (1980, 1989), (1990, 1999), (2000, 2009), (2010, 2019)]
 
+def split_albums_by_decade(path):    
     dirs_splitted_by_decades = {}
 
     for decade in DECADES:
@@ -68,8 +73,37 @@ def get_ranking_for_decade(dataset_path, decade):
     print len(decade_songs)
 
 
+
+def sample_songs_for_decade(path, decade, n_samples):
+    all_albums = split_albums_by_decade(path)
+    for decade_range in DECADES:
+        if decade in range(decade_range[0], decade_range[1]+1):
+            target_decade = decade_range
+
+    all_files = []
+
+    for decade_albums in all_albums[target_decade]:
+        for root, dirs, files in os.walk(decade_albums):  
+            for filename in files:
+                if filename.endswith("mp3") or filename.endswith("wma") or filename.endswith("wav"):
+                    all_files.append('%s/%s' % (decade_albums, filename))
+
+    return random.sample(all_files, n_samples)
+
+
 def main():
-    get_ranking_for_decade('/local/datasets/forro em vinil', 1990)
+    sampled_songs = sample_songs_for_decade('/local/datasets/forro em vinil', 2000, 20)
+
+    for sampled_song in sampled_songs:
+        basename = os.path.basename(sampled_song)
+
+
+        sound = AudioSegment.from_mp3(sampled_song)
+
+        sound.export("/tmp/audio-processing/%s.wav" % basename, format="wav")
+        audioAnalysis.thumbnailWrapper('/tmp/audio-processing/%s.wav' % basename, 20)
+
+        os.rename("/tmp/audio-processing/*thumb1*", "/home/felipev/Desktop/thumbs-forro-daqui/")
 
 
 if __name__ == '__main__':
